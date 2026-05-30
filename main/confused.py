d5e838e5a4864245c5a6df9db553b6486d865a6b
Nathan <mrcodeyes@gmail.com>
2026-05-30 05:54:07 -0400

    get code and renames

--- main/confused.py
+++ main/confused.py
@@ -3,6 +3,9 @@ import sys
 #get the filename
 filename = sys.argv[1]
 
+# Source - https://stackoverflow.com/a/287944
+# Posted by joeld, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-05-29, License - CC BY-SA 4.0
 #the colors for errors
 class colors:
     fail = '\033[91m'
@@ -11,26 +14,44 @@ class colors:
     under = '\033[4m'
 
 #the interpreter for confused
-class interpreter:
+class language:
 
     #the __init__ for the interpreter
     def __init__(self, file):
+        #the lines of code
+        self.code = {}
+        #the mem for the global vars
         self.global_vars = {}
+        #the filename
         self.filename = file
+        #the line we are on
         self.line = 0
         self.gtgc = False #this means good to get code
         #read the file for the code
-        self.read_file()
-    
-    # Source - https://stackoverflow.com/a/287944
-    # Posted by joeld, modified by community. See post 'Timeline' for change history
-    # Retrieved 2026-05-29, License - CC BY-SA 4.0
+        self.allow_file()
+        #get the code
+        self.get_code()
 
-    def read_file(self):
+    #make an error function to reuse this function prints the message and exits the code
+    def error(self, message):
+        print(colors.fail, colors.under + message + colors.endc)
+        sys.exit(1)
+
+    def allow_file(self):
         if not self.filename.endswith(".cfdy"):
-            print(f"{colors.fail} {colors.under}The file you are trying to run must have the file extension .cfdy{colors.endc}")
-            sys.exit(1)
+            #tell the user that they don't haven the right file extension
+            self.error("The file you are trying to run must have the file extension .cfdy")
         elif self.filename.endswith(".cfdy"):
+            #set gtgc to true so we know to read the file
             self.gtgc = True
+    
+    #the function that is going to read the lines of code and store them for later use
+    def get_code(self):
+        if self.gtgc:
+            #open the file and read the ai model told me to use this encoding
+            with open(self.filename, "r", encoding="utf-8") as f:
+                for i in f:
+                    self.line += 1
+                    self.code[self.line] = i.rstrip()
 
-engine = interpreter(filename)
+engine = language(filename)
